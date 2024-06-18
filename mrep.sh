a94393f4609a84832f5e626cab6b426d7308f7f9
Juan Snow <95362321+juansnow01@users.noreply.github.com>
2024-06-18 19:53:38 +0000

    BIG CHANGES
    1. Added some more context so user knows how the command functions. It is placed in a "usage" function to allow for easy mods as app grows. It will also throw an error if a flag/option is incorrectly used.
    
    2. More Variables: values are stored in variables so as the project grows it can be modified easier.
    
    3. Default values: we used some default values because we are not going to be using...
    
    4. FLAGS: the flags are as follows... c flag is for how many trailing and leading lines you want to display, i flag for case insensitivity.

--- mrep.sh
+++ mrep.sh
@@ -1,8 +1,59 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage $0 <man-page> <search-string>"
+
+usage() {
+    echo "Usage: $0 <man-page> <search-string> [-c <context-lines>] [-i]"
+    echo "  <man-page>:       The manual page to search."
+    echo "  <search-string>:  The string to search for."
+    echo "  -c <context-lines>: Number of context lines to show around matches (default: 3)."
+    echo "  -i:               Perform a case-insensitive search."
+    exit 1
+}
+# Default values
+context_lines=3
+case_insensitive=false
+
+if [ "$#" -lt 2 ]; then
+    usage
+fi
+man_page="$1"
+search_string="$2"
+
+shift 2
+
+while getopts ":c:i" opt; do
+    case ${opt} in
+        c)
+            context_lines=$OPTARG
+            ;;
+        i)
+            case_insensitive=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            ;;
+    esac
+done
+
+if ! man -w "$man_page" > /dev/null 2>&1; then
+    echo "Error: The man page for '$man_page' does not exist."
     exit 1
 fi
 
-man $1 | grep --color=always -i -C 3 "$2"
+grep_command="grep --color=always -C $context_lines"
+
+if $case_insensitive; then
+    grep_command+=" -i"
+fi
+
+man "$man_page" | eval "$grep_command \"$search_string\""
+
+if [ $? -ne 0 ]; then
+    echo "No matches found for '$search_string' in the man page for '$man_page'."
+    exit 1
+fi
